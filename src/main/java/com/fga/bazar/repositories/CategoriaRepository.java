eddfa59ef88b0f83fec698fc8781851dbc12dcde
biancasofia <sofiabrasil44@gmail.com>
2022-07-28 14:54:18 -0300

    crud categoria

---
+++ src/main/java/com/fga/bazar/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.fga.bazar.repositories;
+
+import com.fga.bazar.models.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
