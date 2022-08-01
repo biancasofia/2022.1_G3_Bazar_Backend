d3c6847557ce2840fac8d1eb2c5a5a9cf72ac33c
biancasofia <sofiabrasil44@gmail.com>
2022-08-01 16:05:18 -0300

    crud de produto sem categoria

---
+++ src/main/java/com/fga/bazar/models/Produto.java
@@ -0,0 +1,53 @@
+package com.fga.bazar.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100, nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private float preco;
+
+    public Produto(){
+
+    }
+
+    public Produto(Integer id, String nome, float preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+}
