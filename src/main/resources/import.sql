1483400d1e169f6043b9ad47055e73b357708d56
Douglas Monteles <silvadouglas328@gmail.com>
2022-09-08 21:09:22 -0300

    gerando o script da base de dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,24 +11,20 @@ INSERT INTO usuario_papeis (usuario_id, papel_id) VALUES (2, 2);
 INSERT INTO usuario_papeis (usuario_id, papel_id) VALUES (2, 3);
 
 INSERT INTO estado (sigla, nome) VALUES ('DF', 'Distrito Federal');
+INSERT INTO estado (sigla, nome) VALUES ('GO', 'Goiás');
 
 INSERT INTO cidade (nome, estado_id) VALUES ('Brasília', 1);
 INSERT INTO cidade (nome, estado_id) VALUES ('Recanto das Emas', 1);
+INSERT INTO cidade (nome, estado_id) VALUES ('Taguatinga', 1);
+INSERT INTO cidade (nome, estado_id) VALUES ('Vicente Pires', 1);
+INSERT INTO cidade (nome, estado_id) VALUES ('Gama', 1);
+INSERT INTO cidade (nome, estado_id) VALUES ('Valraíso', 2);
 
-INSERT INTO endereco (bairro, cep, numero, cidade_id, usuario_id) VALUES ('Recanto das Emas', 72600400, 12, 1, 1);
+INSERT INTO endereco (bairro, cep, numero, cidade_id, usuario_id) VALUES ('Quadra 400 conjunto 06', 78600400, 12, 1, 1);
+INSERT INTO endereco (bairro, cep, numero, cidade_id, usuario_id) VALUES ('Quandra 600 conjunto 02', 72600400, 30, 1, 1);
+INSERT INTO endereco (bairro, cep, numero, cidade_id, usuario_id) VALUES ('Quandra 600 conjunto 02', 72600400, 30, 1, 2);
 
 INSERT INTO categoria (nome, icone) VALUES ('Informática', 'computer');
 INSERT INTO categoria (nome, icone) VALUES ('Roupas', 'checkroom');
 INSERT INTO categoria (nome, icone) VALUES ('Acessórios', 'headphones_battery');
-
-INSERT INTO produto (nome, preco) VALUES ('Xiaomi Redmi 11s', 1475.00);
-INSERT INTO produto (nome, preco) VALUES ('Xiaomi POCO M3', 1350.00);
-INSERT INTO produto (nome, preco) VALUES ('Pulseira de ouro', 10500.00);
-INSERT INTO produto (nome, preco) VALUES ('Brincos', 10500.00);
-INSERT INTO produto (nome, preco) VALUES ('Trevesseiro', 50.00);
-
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUES (1, 1);
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUES (2, 1);
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUES (3, 2);
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUES (4, 2);
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUES (5, 3);
+INSERT INTO categoria (nome, icone) VALUES ('Eletrônicos', 'on_device_training');
